adbb4ae4a8d8a8404728b6342dea89bff4ffb595
WWJiaxuan <52562219+WWJiaxuan@users.noreply.github.com>
2021-05-20 16:03:19 +0800

    Update __init__.py

--- scAMACE_py/__init__.py
+++ scAMACE_py/__init__.py
@@ -1,5 +1,1 @@
-from .generate_sim_data import *
-from .model_based_torch_cpu import *
-from .model_based_torch_gpu import *
-from .model_based_sep_torch_cpu import *
-from .model_based_sep_torch_gpu import *
+
